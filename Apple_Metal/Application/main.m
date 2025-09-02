b84a09dc779b2259c6fbe7346d942100d2d4117e
matthegg5 <matthew.heggie@googlemail.com>
2025-09-02 20:25:19 +0100

    updates to project structure

---
+++ Apple_Metal/Application/main.m
@@ -0,0 +1,10 @@
+//
+//  main.m
+//  Apple_Metal
+//
+
+#import "AppKit/AppKit.h"
+
+int main(int argc, const char * argv[]) {
+  return NSApplicationMain(argc, argv);
+}
---
+++ New Folder/Apple_Metal/AppDelegate.h
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.h
+//  Apple_Metal
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
